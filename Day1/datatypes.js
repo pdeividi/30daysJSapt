f69c101c16abb16d8449277935e1f9a2a0cb55ef
Deividi Gonçalves <35402697+pdeividip@users.noreply.github.com>
2021-06-29 11:55:24 -0300

    Add files via upload

---
+++ Day1/datatypes.js
@@ -0,0 +1,13 @@
+let a = 'string'
+let b = true
+let c 
+let d = null
+
+console.log(typeof(a))
+ string
+console.log(typeof(b))
+ boolean
+console.log(typeof(c))
+ undefined
+console.log(typeof(d))
+ object
---
+++ Day1/variable.js
@@ -0,0 +1,4 @@
+let a = 'string'
+let b = true
+let c 
+let d = null
